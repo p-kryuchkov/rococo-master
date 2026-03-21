9520fb5cac62ef9e86e8cecca8660cd2ee715e6c
Pavel Kryuchkov <krutestkru@gmail.com>
2026-03-21 16:24:43 +0300

    Переписал сервисы под разные профили, начал собирать docker compose

--- localenv.sh
+++ localenv.sh
@@ -5,4 +5,4 @@ docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 
 echo '### Run databases ###'
-docker run --name rococo-all-db -p 3306:3306 -e MYSQL_ROOT_PASSWORD=secret -v rangiffler:/var/lib/mysql -d mysql:8.4.7
+docker run --name rococo-all-db -p 3306:3306 -e MYSQL_ROOT_PASSWORD=secret -v rococo:/var/lib/mysql -d mysql:8.4.7
